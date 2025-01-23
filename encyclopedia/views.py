844a5fad716249f528465773ed87de89efcce74f
joao.ottoni <jvoo2016@gmail.com>
2025-01-23 20:32:15 -0300

    First requirement finished, creation of wiki/TITLE path for entries

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,8 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def shows_content(request, page_title):
+    return render(request, "encyclopedia/content.html", {
+        "page_body": util.get_entry(page_title),
+        "title": page_title.capitalize()
+    })
