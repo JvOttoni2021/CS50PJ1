b710407da6d2378c016ed1ce9e5f4b99bb98a105
joao.ottoni <jvoo2016@gmail.com>
2025-01-23 21:12:06 -0300

    Third requirement, search form

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -4,8 +4,24 @@ from . import util
 
 
 def index(request):
+    full_list = util.list_entries()
+    shown_list = list()
+
+    if request.method == "POST":
+        search = request.POST.get('q')
+
+        search_page_content = util.get_entry(search)
+
+        if search_page_content is not None:
+            return shows_content(request, search)
+
+        shown_list = util.list_entries(search)
+
+    if len(shown_list) == 0:
+        shown_list = full_list
+
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": shown_list
     })
 
 def shows_content(request, page_title):
